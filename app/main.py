019cb2e6d6d164d55c75d435427fd3630506b103
adjihyphen <adji.hendrawan@moneyherogroup.com>
2025-08-28 02:24:24 +0700

    CORS set to all

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routing.article import router
 app = FastAPI()
 
 @app.get("/")
 async def index():
     return { "message" : "Just testing the line .." }
+
+
+#CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router=router)
